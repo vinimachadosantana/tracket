6c478c9c892fb6c4a92efd8556f6489ed53c4fa1
vinimachadosantana <vinimachadosantana@gmail.com>
2026-07-30 19:41:52 -0300

    feat: add github and google oauth providers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [],
+  providers: [GitHub, Google],
 });
